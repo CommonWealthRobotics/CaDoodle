7abf32cd610999752a7f63e14a4853d627a349ad
Kevin harrington <mad.hephaestus@gmail.com>
2023-09-20 17:24:39 -0400

    adding basic release tools

---
+++ application/src/main/java/com/commonwealthrobotics/NameGetter.java
@@ -0,0 +1,25 @@
+package com.commonwealthrobotics;
+
+import java.util.function.Supplier;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class NameGetter  implements Supplier<String>{
+
+	@Override
+	public String get() {
+		String sec = "REPLACE_ME";
+		if(sec.contains("REPLACE")) {
+			String line = System.getProperty("API-SECRET");
+			if(line!=null)
+				return line;
+			JFrame jframe = new JFrame();
+			String answer = JOptionPane.showInputDialog(jframe, "Enter API secret");
+			jframe.dispose();
+			return answer;
+		}
+		return sec;
+	}
+
+}
