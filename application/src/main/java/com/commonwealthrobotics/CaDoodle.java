76f49e1a5f2feadb969947baafc292f55b74e1d9
Kevin harrington <mad.hephaestus@gmail.com>
2023-09-21 08:17:25 -0400

    adding the build info into the print statement

--- application/src/main/java/com/commonwealthrobotics/CaDoodle.java
+++ application/src/main/java/com/commonwealthrobotics/CaDoodle.java
@@ -4,5 +4,8 @@ public class CaDoodle{
 
 	public static void main(String [] args){
 		System.out.println("Launching Cadoodle application");
+		System.out.println("Build info "+StudioBuildInfo.getVersion());
+		System.out.println("OS "+System.getProperty("os.name"));
+		System.out.println("Archetecture "+System.getProperty("os.arch"));
 	}
 }
