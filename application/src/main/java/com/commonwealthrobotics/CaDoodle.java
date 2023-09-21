16e512d15b5d3fdff41daba044838ef0684bab7c
Kevin harrington <mad.hephaestus@gmail.com>
2023-09-21 08:03:37 -0400

    adding a print statement

--- application/src/main/java/com/commonwealthrobotics/CaDoodle.java
+++ application/src/main/java/com/commonwealthrobotics/CaDoodle.java
@@ -3,6 +3,6 @@ package com.commonwealthrobotics;
 public class CaDoodle{
 
 	public static void main(String [] args){
-		System.out.println("Launching Cadoodle updater");
+		System.out.println("Launching Cadoodle application");
 	}
 }
