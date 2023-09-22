40f05ca1cdb22a437bc03a5806fb56791fa81200
Kevin Harrington <mad.hephaestus@gmail.com>
2023-09-22 17:12:04 -0400

    updating name of jar in workflow

--- updater/src/main/java/com/commonwealthrobotics/CaDoodleUpdater.java
+++ updater/src/main/java/com/commonwealthrobotics/CaDoodleUpdater.java
@@ -1,11 +1,15 @@
 package com.commonwealthrobotics;
 
 public class CaDoodleUpdater{
-
+	private static String project = "CommonWealthRobotics";
+	private static String repo = "CaDoodle";
+	
+	private static String baseURL = "https://github.com/" + project + "/" + repo ;
 	public static void main(String [] args){
 		System.out.println("Launching Cadoodle Updater");
 		System.out.println("Build info "+StudioBuildInfo.getVersion());
 		System.out.println("OS "+System.getProperty("os.name"));
 		System.out.println("Archetecture "+System.getProperty("os.arch"));
+		
 	}
 }
