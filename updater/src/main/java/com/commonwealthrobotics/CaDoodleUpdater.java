e28ff3be63cf207f635ff35a09c29c4b01db9d2a
Kevin Harrington <mad.hephaestus@gmail.com>
2023-09-23 10:46:19 -0400

    adding spotless to the build

--- updater/src/main/java/com/commonwealthrobotics/CaDoodleUpdater.java
+++ updater/src/main/java/com/commonwealthrobotics/CaDoodleUpdater.java
@@ -1,15 +1,18 @@
+/* (C)2023 */
 package com.commonwealthrobotics;
 
-public class CaDoodleUpdater{
-	private static String project = "CommonWealthRobotics";
-	private static String repo = "CaDoodle";
-	
-	private static String baseURL = "https://github.com/" + project + "/" + repo ;
-	public static void main(String [] args){
-		System.out.println("Launching Cadoodle Updater");
-		System.out.println("Build info "+StudioBuildInfo.getVersion());
-		System.out.println("OS "+System.getProperty("os.name"));
-		System.out.println("Archetecture "+System.getProperty("os.arch"));
-		
-	}
+public class CaDoodleUpdater {
+  private static String project = "CommonWealthRobotics";
+  private static String repo = "CaDoodle";
+  private static String jvm = "cadoodle-jvm-configuration.json";
+
+  private static String baseURL = "https://github.com/" + project + "/" + repo;
+
+  public static void main(String[] args) {
+    System.out.println("Launching Cadoodle Updater");
+    System.out.println("Build info " + StudioBuildInfo.getVersion());
+    System.out.println("OS " + System.getProperty("os.name"));
+    System.out.println("Archetecture " + System.getProperty("os.arch"));
+    System.out.println("Build Date " + StudioBuildInfo.getBuildDate());
+  }
 }
