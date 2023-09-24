56884343ba7ade94cc9442275b664aaf104ece36
Kevin Harrington <mad.hephaestus@gmail.com>
2023-09-24 16:27:37 -0400

    updating the updater to add additional VMs

--- updater/src/main/java/com/commonwealthrobotics/CaDoodleUpdater.java
+++ updater/src/main/java/com/commonwealthrobotics/CaDoodleUpdater.java
@@ -1,6 +1,7 @@
 /* (C)2023 */
 package com.commonwealthrobotics;
 
+
 public class CaDoodleUpdater {
 	private static String project = "CommonWealthRobotics";
 	private static String repo = "CaDoodle";
@@ -8,11 +9,13 @@ public class CaDoodleUpdater {
 
 	private static String baseURL = "https://github.com/" + project + "/" + repo;
 
-	public static void main(String[] args) {
+	public static void main(String[] a) {
 		System.out.println("Launching Cadoodle Updater");
 		System.out.println("Build info " + StudioBuildInfo.getVersion());
 		System.out.println("OS " + System.getProperty("os.name"));
 		System.out.println("Archetecture " + System.getProperty("os.arch"));
 		System.out.println("Build Date " + StudioBuildInfo.getBuildDate());
+		String[] args = new String[] {project,repo,"CaDoodle.jar",jvm,"-Dprism.forceGPU=true","-jar"};
+		LatestFromGithubLaunch.Main.main(args);
 	}
 }
