6ce14a60d8d0c4bf54fa2004809e691d77a01eb7
Kevin Harrington <mad.hephaestus@gmail.com>
2023-09-24 18:50:08 -0400

    launch BowlerStudio to start

--- application/src/main/java/com/commonwealthrobotics/CaDoodle.java
+++ application/src/main/java/com/commonwealthrobotics/CaDoodle.java
@@ -1,5 +1,7 @@
 package com.commonwealthrobotics;
 
+import com.neuronrobotics.bowlerstudio.BowlerStudio;
+
 public class CaDoodle{
 
 	public static void main(String [] args){
@@ -7,6 +9,11 @@ public class CaDoodle{
 		System.out.println("Build info "+StudioBuildInfo.getVersion());
 		System.out.println("OS "+System.getProperty("os.name"));
 		System.out.println("Archetecture "+System.getProperty("os.arch"));
-		
+		try {
+			BowlerStudio.main(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
