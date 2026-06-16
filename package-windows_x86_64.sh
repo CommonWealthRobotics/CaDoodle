da55baf96c1f20430aa69137db0666629c8dff70
Kevin Harrington <mad.hephaestus@gmail.com>
2026-06-16 11:30:36 -0400

    use powershell for makeappx

--- package-windows_x86_64.sh
+++ package-windows_x86_64.sh
@@ -102,9 +102,8 @@ sed -i "s/Version=\"1.0.0.0\"/Version=\"$VERSION_QUAD\"/" MsixStage/AppxManifest
 #echo "Found makeappx at: $MAKEAPPX"
 #"$MAKEAPPX" pack /d MsixStage /p "$NAME-$VERSION.msix" /nv
 MAKEAPPX=$(find "C:/Program Files (x86)/Windows Kits/10/bin" -name "makeappx.exe" -path "*/x64/*" | sort -V | tail -1)
-MAKEAPPX_WIN=$(cygpath -w "$MAKEAPPX")
-echo "Found makeappx at: $MAKEAPPX_WIN"
-cmd //c "\"$MAKEAPPX_WIN\" pack /d MsixStage /p $NAME-$VERSION.msix /nv /o"
+echo "Found makeappx at: $MAKEAPPX"
+powershell.exe -Command "& '$MAKEAPPX' pack /d MsixStage /p $NAME-$VERSION.msix /nv /o"
 
 cp "$NAME-$VERSION.msix" "release/$NAME-Windows-$ARCH.msix"
 
