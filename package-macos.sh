961702d43359d190a6f5db820e9aaa730821d772
Kevin Harrington <mad.hephaestus@gmail.com>
2026-08-21 12:26:08 -0400

    try to extract the plugins into the application

--- package-macos.sh
+++ package-macos.sh
@@ -43,9 +43,11 @@ echo "Test jar complete"
 
 cp zulu*jdk*-macosx_*.zip $DIR/
 cp CaDoodle-ApplicationInstall.zip $DIR/
-#if [[ $(uname -m) == 'arm64' ]]; then
-#    cp BowlerStudioInstall.zip $DIR/
-#fi
+if [[ $(uname -m) == 'arm64' ]]; then
+    unzip -o BowlerStudioInstall.zip -d $DIR/
+fi
+echo "\n\nTarget Dir: $DIR"
+ls -al $DIR/
 
 ICON=$NAME.png
 cp SourceIcon.png $ICON
