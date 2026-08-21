2b3b6c42348e1b0aa882097e4fe3de154b6b0a39
Kevin Harrington <mad.hephaestus@gmail.com>
2026-08-21 16:16:52 -0400

    cant bundle plugins

--- package-macos.sh
+++ package-macos.sh
@@ -45,7 +45,7 @@ cp zulu*jdk*-macosx_*.zip $DIR/
 cp CaDoodle-ApplicationInstall.zip $DIR/
 if [[ $(uname -m) == 'arm64' ]]; then
     mkdir $DIR/BowlerStudioInstall/
-    unzip -q -o BowlerStudioInstall-macos-arm.zip -d $DIR/BowlerStudioInstall/
+    #unzip -q -o BowlerStudioInstall-macos-arm.zip -d $DIR/BowlerStudioInstall/
 fi
 echo "\n\nTarget Dir: $DIR"
 ls -al $DIR/
